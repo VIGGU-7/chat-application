f68d707f75745c15973c6f080eaaf920691ff875
VIGGU <vigneshpatlavath@gmail.com>
2025-07-05 11:45:38 +0530

    refactor:same site to none for production

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ const token=jwt.sign({userId},process.env.JWT_SECRET,{
 res.cookie("token",token,{
     maxAge:7*24*60*60*1000,
     httpOnly:true,
-    sameSite:true,
+    sameSite:none,
     secure:process.env.NODE_ENV !=="devolopment"
 })
 
